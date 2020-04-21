3cc31e018a7a50917d9806567f36db6323350e6a
zxf000000 <zhouxiaofei0001@gmail.com>
2020-04-21 11:14:57 +0800

    创建新项目

---
+++ WriteReadMedia/WriteReadMedia/WriteReadMedia/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  WriteReadMedia
+//
+//  Created by 壹九科技1 on 2020/4/21.
+//  Copyright © 2020 zxf. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+
+}
+
---
+++ WriteReadMedia/WriteReadMedia/WriteReadMedia/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WriteReadMedia
+//
+//  Created by 壹九科技1 on 2020/4/21.
+//  Copyright © 2020 zxf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
